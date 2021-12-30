a54ac34eb8ff32b9d4ea3e18c3597f84e2dc4511
DavidS415 <93840316+DavidS415@users.noreply.github.com>
2021-12-30 13:31:31 -0800

    Status update won't redirct user

--- scripts/changestatusscript.php
+++ scripts/changestatusscript.php
@@ -15,9 +15,7 @@ require_once "../loginsys/config.php";
 $sql = "update new_obrfs set current_status='$current_status' WHERE tracker_placement_id = '$current_tracker_id'";
 
 if ($mysqli->query($sql) === TRUE) {
-	echo "Status updated";
-    echo "<p>Click <a href='../index.php'>here</a> to return to home and view OBRFs</p>";
-    echo "<p>Click <a href='../form.php'>here</a> to submit a new OBRF</p>";
+    header("location: ../edit.php");
 } else {
 	echo "Error: ".$sql."<br>".$conn->error;
 }
